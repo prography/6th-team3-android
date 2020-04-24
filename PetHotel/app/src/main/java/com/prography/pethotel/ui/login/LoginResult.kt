ee42e1c705c65272aaa515554249fd14804ac63b
gahee <sarahan774@gmail.com>
2020-04-24 17:35:41 +0900

    Generated Signed Bundle

---
+++ PetHotel/app/src/main/java/com/prography/pethotel/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.prography.pethotel.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
