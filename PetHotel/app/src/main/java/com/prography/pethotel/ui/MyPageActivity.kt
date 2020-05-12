44adad8d42579eb99c0b1fdc3ef7a64e5c1dc53a
LeeYeJin <kains0417@naver.com>
2020-05-12 20:58:40 +0900

    update

---
+++ PetHotel/app/src/main/java/com/prography/pethotel/ui/MyPageActivity.kt
@@ -0,0 +1,14 @@
+package com.prography.pethotel.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.prography.pethotel.R
+
+class MyPageActivity :AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page)
+
+    }
+}
