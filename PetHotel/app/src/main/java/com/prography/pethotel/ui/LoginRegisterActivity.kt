37f07c193e3757cf5f9a545fc9bbf766eebe677f
gahee <sarahan774@gmail.com>
2020-05-13 18:04:55 +0900

    Update Login Register Fragment

--- PetHotel/app/src/main/java/com/prography/pethotel/ui/LoginRegisterActivity.kt
+++ PetHotel/app/src/main/java/com/prography/pethotel/ui/LoginRegisterActivity.kt
@@ -11,15 +11,8 @@ class LoginRegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.testing_input_field_layout)
+        setContentView(R.layout.activity_login_register)
 
-
-        input_box_name.title_field.text = "사용자 이름"
-        input_box_phone_number.title_field.text = "전화번호"
-        input_box_year.title_field.text = "태어난 년도"
-
-        input_box_name.edit_text_field.inputType = EditorInfo.TYPE_TEXT_VARIATION_PERSON_NAME
-        input_box_name.edit_text_field.hint = "이름을 입력해 주세요"
 
     }
 
