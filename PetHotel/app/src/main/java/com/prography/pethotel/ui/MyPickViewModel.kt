f8d15b2b7f1d656db64b2c87fd02a94d962d0040
LeeYeJin <kains0417@naver.com>
2020-05-20 16:35:37 +0900

    Add MyPick ViewModel, xml

---
+++ PetHotel/app/src/main/java/com/prography/pethotel/ui/MyPickViewModel.kt
@@ -0,0 +1,15 @@
+package com.prography.pethotel.ui
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+
+class MyPickViewModel(application: Application) : AndroidViewModel(application){
+//    private val model = MyPickModel(application)
+//    private val petplaces = model.getMyPickHotels()
+//
+//    fun getHotels(): LiveData<List<PetPlace>>{
+//        return this.petplaces
+//    }
+}
