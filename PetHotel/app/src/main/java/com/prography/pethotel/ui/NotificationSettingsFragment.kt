73d397a3c1d1c0ad27ca89905ca4a8d7bbfa0215
LeeYeJin <kains0417@naver.com>
2020-05-23 17:34:33 +0900

    Add mypage package

--- PetHotel/app/src/main/java/com/prography/pethotel/ui/NotificationSettingsFragment.kt
+++
@@ -1,5 +0,0 @@
-package com.prography.pethotel.ui
-
-class NotificationSettingsFragment {
-
-}
--- PetHotel/app/src/main/java/com/prography/pethotel/ui/mypage/MyPickViewModel.kt
+++ PetHotel/app/src/main/java/com/prography/pethotel/ui/mypage/MyPickViewModel.kt
@@ -1,4 +1,4 @@
-package com.prography.pethotel.ui
+package com.prography.pethotel.ui.mypage
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
