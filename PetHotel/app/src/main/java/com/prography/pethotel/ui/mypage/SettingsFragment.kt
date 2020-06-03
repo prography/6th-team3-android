52adb8c524d5330f014257c45135e11b71c15928
LeeYeJin <kains0417@naver.com>
2020-06-03 19:22:20 +0900

    update

--- PetHotel/app/src/main/java/com/prography/pethotel/ui/mypage/SettingsFragment.kt
+++ PetHotel/app/src/main/java/com/prography/pethotel/ui/mypage/SettingsFragment.kt
@@ -1,4 +1,12 @@
 package com.prography.pethotel.ui.mypage
 
-class SettingsFragment {
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import com.prography.pethotel.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings_screen, rootKey)
+    }
 }
