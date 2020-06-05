74b393c285c0951653fc6a19a836c7b1778ca916
gahee <sarahan774@gmail.com>
2020-06-05 17:28:58 +0900

    update place fragment

--- PetHotel/app/src/main/java/com/prography/pethotel/ui/places/PlaceInfoDetailsFragment.kt
+++ PetHotel/app/src/main/java/com/prography/pethotel/ui/places/PlaceInfoDetailsFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import com.prography.pethotel.R
-
+import com.prography.pethotel.models.Hotel
+import kotlinx.android.synthetic.main.place_info_detail_descriptions_layout.*
+import kotlinx.android.synthetic.main.place_info_detail_time_etc_layout.*
 
 
 class PlaceInfoDetailsFragment : Fragment() {
@@ -18,6 +21,53 @@ class PlaceInfoDetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_place_info_details, container, false)
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val hotel : Hotel = arguments?.get("hotel") as Hotel
+
+        place_detail_name.text = hotel.hotelName
+        place_detail_address.text = hotel.addressDetail
+        btn_place_detail_info.setOnClickListener {
+            Toast.makeText(requireContext(), hotel.pageLink, Toast.LENGTH_SHORT).show()
+        }
+        btn_place_detail_like.setOnClickListener {
+            Toast.makeText(requireContext(), "저장되었습니다!", Toast.LENGTH_SHORT).show()
+        }
+        btn_place_detail_phone.setOnClickListener {
+            Toast.makeText(requireContext(), hotel.phoneNumber, Toast.LENGTH_SHORT).show()
+        }
+        tv_place_detail.text = hotel.description
+
+        val weekTime = "${hotel.weekOpenTime} ~ ${hotel.weekCloseTime}"
+        val satTime = "${hotel.satOpenTime} ~ ${hotel.satCloseTime}"
+        val sunTime = "${hotel.sunOpenTime} ~ ${hotel.sunCloseTime}"
+        title_week_time.append(weekTime)
+        title_sat_time.append(satTime)
+        title_sun_time.append(sunTime)
+
+        when {
+            hotel.monitorAvailable -> {
+                tv_monitoring_contents.text = "제공"
+            }
+            else -> {
+                tv_monitoring_contents.text = "미제공"
+            }
+        }
+
+        when{
+            hotel.isNeuteredOnly -> {
+                tv_neuter_needed.text = "중성화 필요"
+            }else -> {
+                tv_neuter_needed.text = "중성화 필요 없음"
+            }
+        }
+
+        tv_week_price.append("${hotel.weekPrice} 원")
+        tv_sat_price.append("${hotel.satPrice} 원")
+        tv_sun_price.append("${hotel.sunPrice} 원")
     }
 
 
--- PetHotel/app/src/main/java/com/prography/pethotel/ui/places/PlaceInfoFragment.kt
+++ PetHotel/app/src/main/java/com/prography/pethotel/ui/places/PlaceInfoFragment.kt
@@ -6,12 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.prography.pethotel.R
+import com.prography.pethotel.models.Hotel
+import kotlinx.android.synthetic.main.place_info_fragment.*
 
 class PlaceInfoFragment : Fragment() {
 
     private lateinit var viewModel: PlaceInfoViewModel
+    private var hotelList : ArrayList<Hotel> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,7 +27,41 @@ class PlaceInfoFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(PlaceInfoViewModel::class.java)
-     
+
+        //recycler view 만들기
+
+        //filtering 하는 기능 제공하기.
+        //1. near by places
+        //2.monitor providers
+        //3. price discounts (?)
+
+        makeDummyHotelInfo()
+
+        val placeInfoAdapter = PlaceListAdapter(requireContext(), hotelList)
+        rv_place_info.apply {
+            adapter = placeInfoAdapter
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+
+    private fun makeDummyHotelInfo(){
+        if(hotelList.isNotEmpty()){
+            return
+        }else{
+            for(x in 0 .. 20){
+                hotelList.add(
+                    Hotel(x, "", "", "애견호텔 $x",
+                        "This is a dummy description #$x", "서울시 용산구 $x 길 $x ",
+                        "Dummy address detail #$x", "dummy zip code #$x",
+                        "30303040", "234234234", "09:00",
+                        "20:00", "09:00", "18:00",
+                        "09:00", "18:00", 10000, 20000, 20000,
+                        "010-1123-1231", true, true, 3, "www.dummylink.com")
+                )
+            }
+        }
     }
 
 }
